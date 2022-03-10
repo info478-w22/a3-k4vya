8b4c143abc7a963eabaac8af838442f0e25b51b7
k4vya <kavyai@uw.edu>
2022-03-09 20:42:55 -0800

    PAIN

---
+++ app_ui.R
@@ -0,0 +1,60 @@
+library(shiny)
+library(shinythemes)
+
+# Page One
+page_one <- tabPanel(
+  "Introduction",
+  mainPanel(   
+    h1("Deterministic v Schoastic Models of Covid"),
+    p("Kavya Iyer, A3"),
+    h4("Analysis"), 
+    p("After doing research, I saw how effective at reducing the transmission rate options such as indoor masking and social distancing were. It was interesting to see that as these filters are applied to both models, the amount
+      of time for the pandemic to level out was really reduced."),
+    h5("Sources Used"),
+    p("https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7976050/#:~:text=The%20maximum%20probability%20of%2063.2,in%20order%20to%20avoid%20infection."),
+    p("https://www.science.org/doi/10.1126/science.abg6296")
+  ))
+# Widgets 
+
+# Page 2
+page_two <- tabPanel(
+  "Deterministic Model",
+  h1("COVID 19 Deterministic Model"),
+  sidebarLayout(             
+    sidebarPanel(selectInput(
+      inputId = "dcm_choice",
+      label = "Choose Value",
+      
+      choices = c("None", "Mask Indoors", "Social Distancing"),
+      selected = "None"
+    )),           
+    mainPanel(   
+      plotlyOutput("det"),
+    )))
+
+# Page Three
+page_three <- tabPanel(
+  "Stochastic Model",
+  h1("COVID 19 Stochastic Model"),
+  sidebarLayout(             
+    sidebarPanel(selectInput(
+      inputId = "stc_choice",
+      label = "Choose Value",
+      
+      choices = c("None", "Mask Indoors", "Social Distancing"),
+      selected = "None"
+    )),           
+    mainPanel(   
+      plotlyOutput("stc"),
+    )))
+
+
+
+
+# Define the UI and what pages/tabs go into it
+ui <- navbarPage(theme = shinytheme("journal"),
+                 "INFO 478 A3",
+                 page_one,
+                 page_two, 
+                 page_three
+)
---
+++ meep.R
@@ -0,0 +1,60 @@
+#install.packages("dcm")
+#install.packages("EpiModel", dependencies = TRUE)
+
+require(EpiModel)
+#require(dcm)
+library(tidyverse)
+#################################################################################
+#### Deterministic compartmental models
+#################################################################################
+
+######## Basic SI model 
+
+
+
+
+#################################################################################
+#### Individual contact models (and comparison to DCM)
+#################################################################################
+
+
+# Deterministic
+
+server <- function(input, output) {
+        dcm <- read.csv("data/dcm.csv")
+        output$det <- renderPlotly({
+                
+                selected_data <- dcm %>% 
+                        filter(type == input$dcm_choice)
+                
+                param.dcm <- param.dcm(inf.prob = c(selected_data$inf), act.rate = selected_data$act, rec.rate = 0.07)
+                init.dcm <- init.dcm(s.num = 500, i.num = 1,r.num = 0)
+                control.dcm <- control.dcm(type = "SIR", nsims = 10, nsteps = 300)
+                mod.dcm <- dcm(param.dcm, init.dcm, control.dcm)
+                
+                return(plot(mod.dcm))
+                
+        })
+        
+        output$stc <- renderPlotly({
+                selected_data <- dcm %>% 
+                        filter(type == input$stc_choice)
+                
+                #Stochastic 
+                param.icm <- param.icm(inf.prob = c(selected_data$inf), act.rate = selected_data$act, rec.rate = 0.07)
+                init.icm <- init.icm(s.num = 500, i.num = 1,r.num = 0)
+                control.icm <- control.icm(type = "SIR", nsims = 100, nsteps = 300)
+                mod.icm <- icm(param.icm, init.icm, control.icm)
+                stc_model<- plot(mod.icm)
+                return(stc_model)
+                
+        })
+        
+        
+        }
+
+
+
+
+
+
